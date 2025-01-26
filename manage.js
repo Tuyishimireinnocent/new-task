60b18c45b6e2e7849375a53e86644276ff2a0783
Tuyishimireinnocent <142543659+Tuyishimireinnocent@users.noreply.github.com>
2025-01-26 22:08:00 +0200

    delete

--- manage.js
+++ manage.js
@@ -34,12 +34,21 @@ const displayContact = () =>{
                     <td>${contact.email}</td>
                     <td class="actions">
                         <button class="edit">Edit</button>
-                        <button class="delete">Delete</button>
+                        <button class="delete" onclick = "deleteContact(event,${index})">Delete</button>
                     </td>
                 </tr>`
 
         contactDivision.innerHTML += contactCard  
     });  
 }
+function deleteContact(e,index){
+    let allowDelete = confirm('are you sure for to delete this data?')
+    contacts = JSON.parse(localStorage.getItem("contacts")) 
+    contacts.splice(index,1);
+    saveContact()
+    displayContact()   
+}
+
+document.deleteContact = deleteContact
 displayContact()
 
